f8277c2679933f6e4128709d6cb0a93d6c354f6b
Abbes <benabbesmohamed2002@gmail.com>
2025-02-08 13:37:32 +0100

    successful deployment

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,10 +9,11 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const server_socket = "http://localhost:4000/";
-const socketId = socketIO(server_socket, { transports: ["websocket"] });
+import { socket } from "../../server";
+
+const socketId = socketIO(socket, { transports: ["websocket"] });
 const DashboardMessages = () => {
-  const { seller,isLoading } = useSelector((state) => state.seller);
+  const { seller, isLoading } = useSelector((state) => state.seller);
   const [conversations, setConversations] = useState([]);
   const [arrivalMessage, setArrivalMessage] = useState(null);
   const [currentChat, setCurrentChat] = useState();
@@ -259,7 +260,7 @@ const MessageList = ({
   setUserData,
   online,
   setActiveStatus,
-  isLoading
+  isLoading,
 }) => {
   console.log(data);
   const [user, setUser] = useState([]);
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,5 @@
 export const server = process.env.REACT_APP_BACKEND_URL
+export const socket = process.env.REACT_APP_SOCKET_URL
 
 
 
